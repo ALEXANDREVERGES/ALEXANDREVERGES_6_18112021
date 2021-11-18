4e8a476f24ac1017c4ca92a1d959b4ee9b5b7f37
Alex <alexandre.verges@live.fr>
2021-11-18 14:34:16 +0100

    cofiguration auth fini

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,19 @@
 const express = require('express');
 //***application express  ce qui permet de créer applmication express******/
 const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
 //***npms install --save mongoose */
 const mongoose = require('mongoose');
+
+const userRoutes = require('./routes/user');
+
 
 // ********** se connecter a mongo ************************************************************************//
 mongoose.connect('mongodb+srv://bounise1:159Alex13@cluster0.1cpua.mongodb.net/Cluster0?retryWrites=true&w=majority',
@@ -30,5 +41,7 @@ app.use((req, res, next) =>{
 app.use((req, res) => {
     console.log('réponse envoyée avec succès !');
 });
+
+app.use('/api/auth', userRoutes);
 //*****exporter application pour qu'on puisse y accéder depuis nos autres fichiers **** */
 module.exports = app;
