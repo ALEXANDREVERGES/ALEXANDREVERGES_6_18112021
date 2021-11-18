fd2710ab41294f37e840c4ae702dced64068dada
Alex <alexandre.verges@live.fr>
2021-11-18 11:20:01 +0100

    first commit

---
+++ backend/app.js
@@ -0,0 +1,34 @@
+//*****Instalation express npm install -g express ***********/
+//** importer express *****/
+const express = require('express');
+//***application express  ce qui permet de créer applmication express******/
+const app = express();
+//***npms install --save mongoose */
+//const mongoose = require('mongoose');
+
+// ********** se connecter a mongo ************************************************************************//
+/*mongoose.connect('mongodb+srv://bounise:159Alex13@cluster0.1cpua.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));*/
+
+
+
+app.use((req, res, next) => {
+    console.log('requête reçue !');
+    next();//*******next() permet envoyer la réponse, sinon la requete ne se termine pas */
+});
+app.use((req, res, next) => {
+    res.status(201);
+    next();
+});
+app.use((req, res, next) =>{
+    res.json({message: 'Votre requête a bien été reçu!'})
+    next();
+});
+app.use((req, res) => {
+    console.log('réponse envoyée avec succès !');
+});
+//*****exporter application pour qu'on puisse y accéder depuis nos autres fichiers **** */
+module.exports = app;
